d78bd0e69496f7d94bc67e304b10784cdfc5827f
Maria Schreiber <meschreiber3@gmail.com>
2017-04-27 18:11:58 -0400

    basic set-up

---
+++ browser/js/components/Root.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import Navbar from './Navbar';
+
+const Root = ({ children }) => (
+  <div id="main" className="container-fluid">
+    <Navbar />
+    { children }
+  </div>
+);
+
+export default Root;
