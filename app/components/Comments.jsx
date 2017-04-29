a48938e297b2673f00ac7466d17bbeb06a51b32f
Maria Schreiber <meschreiber3@gmail.com>
2017-04-29 13:07:23 -0400

    blockquote styling

--- app/components/Comments.jsx
+++ app/components/Comments.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 export default (props) => {
   const comments = props.comments.map((comment) =>
-    <li key={comment.id}>{comment.text}</li>
+    <div key={comment.id}>
+      <li className="comment" >
+        <blockquote>
+          <p>{comment.text}</p>
+        </blockquote>
+        <p id="author"> - {comment.author}</p>
+      </li>
+    </div>
   )
 
   return (
-    <div className="Comments">
+    <div className="comments">
       <ul>
         {comments}
       </ul>
     </div>
   )
 }
-
-
