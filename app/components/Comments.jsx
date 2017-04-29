735996a0ffa8fd0ef0db8de29dab9cd925b13c1a
Maria Schreiber <meschreiber3@gmail.com>
2017-04-29 12:32:01 -0400

    dumb comment component

--- app/components/Comments.jsx
+++ app/components/Comments.jsx
@@ -1,1 +1,16 @@
-export default () => null
+import React from 'react'
+export default (props) => {
+  const comments = props.comments.map((comment) =>
+    <li key={comment.id}>{comment.text}</li>
+  )
+
+  return (
+    <div className="Comments">
+      <ul>
+        {comments}
+      </ul>
+    </div>
+  )
+}
+
+
